f979733e60dddc2f8567d87f758dcf78b9eca174
pengguohang <pengguohang2000@163.com>
2025-05-09 21:41:59 +0800

    refactor: 将数据库配置迁移到环境变量并更新示例文件
    将数据库配置从硬编码迁移到环境变量，以提高安全性和可维护性。同时，更新了 `.env.example` 文件以包含新的配置项，并删除了旧的 `.env` 文件。

--- express/config/db.js
+++ express/config/db.js
@@ -1,29 +1,23 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-// const sequelize = new Sequelize('journio', 'root', 'abc123', {
-//   host: '127.0.0.1',
-//   dialect: 'mysql',
-//   logging: false, // 设置为true可以在控制台看到SQL查询
-//   define: {
-//     // 全局设置表名与模型名相同，不会自动加s
-//     freezeTableName: false,
-//     // 全局设置不自动添加时间戳字段
-//     timestamps: false
-//   }
-// });
-
-const sequelize = new Sequelize('journio', 'root', '', {
-  host: '',
-  port: 1, // 添加端口配置
-  dialect: 'mysql',
-  logging: false, // 设置为true可以在控制台看到SQL查询
-  define: {
-    // 全局设置表名与模型名相同，不会自动加s
-    freezeTableName: false,
-    // 全局设置不自动添加时间戳字段
-    timestamps: false,
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.DB_USERNAME,
+  process.env.PASSWORD,
+  {
+    host: process.env.HOST,
+    port: process.env.PORT, // 添加端口配置
+    dialect: 'mysql',
+    logging: false, // 设置为true可以在控制台看到SQL查询
+    define: {
+      // 全局设置表名与模型名相同，不会自动加s
+      freezeTableName: false,
+      // 全局设置不自动添加时间戳字段
+      timestamps: false,
+    },
   },
-});
+);
 
 // 测试数据库连接
 sequelize
